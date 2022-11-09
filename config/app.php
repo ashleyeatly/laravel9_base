ad5eb0e528eca46fe512305a8bef0846aa92926f
Ashley Eatly <ashley.eatly@bcs.org>
2022-11-09 22:51:14 +0000

    after adding App\Providers\FortifyServiceProvider::class, to app.php

--- config/app.php
+++ config/app.php
@@ -181,7 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+//        Aloso added for Fortify
+        App\Providers\FortifyServiceProvider::class,
         /*
          * Package Service Providers...
          */
